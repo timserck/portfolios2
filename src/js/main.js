3db705ac360741ddaa0b8981dd1922b2ca95e6a2
server <server@server.com>
2016-11-09 20:34:22 +0100

    'jsImg'

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,45 @@
 const $ = require("jquery");
 
+
+let ProjetsLink = $('.projects__li--a')
+let ProjetsLinks = $('.projects').find(ProjetsLink);
+let ProjectsImg = document.getElementById("projects__img");
+// console.log(ProjetsLinks);
+
+let ProjectId = function(){
+	for (var i = ProjetsLinks.length - 1; i >= 0; i--) {
+		ProjetsLinks[i].id = 'img_' + i;
+		// console.log(ProjetsLinks[i].id);
+	};
+}
+
+let ProjectImg = function(LinkId){
+	let url = 'imgs/'+LinkId + '.jpg'; 
+	ProjectsImg.src = url;
+}
+
+let ProjectHover = function(){
+	ProjetsLink.hover(function(){
+	ProjetsLink.removeClass('active');
+	let linkHover = 'img_' + $('a').index($(this));
+	$('#' + linkHover).addClass('active');
+
+	ProjectImg(linkHover);
+	});
+}
+
+let projectModule = function(){
+	ProjectId();
+	ProjectHover();
+}
+
+projectModule();
+
+
+
+
+
+
 // var module = require('./monModule.js');
 // module.myvar = 'Hello world';
 // module.test();
